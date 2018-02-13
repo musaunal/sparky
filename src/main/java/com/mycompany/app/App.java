aa1154d1907868e42f908c73314928cc6686e973
TheForty <musaunal680@gmail.com>
2018-02-13 16:19:54 +0300

    refactor

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,8 +11,8 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
-public class App{
-
+public class App
+{
     public static boolean search(ArrayList<Integer> array, int e) {
       System.out.println("inside search");
       if (array == null) return false;
@@ -29,13 +29,15 @@ public class App{
         get("/", (req, res) -> "Hello, World");
 
         post("/compute", (req, res) -> {
-          System.out.println(req.queryParams("input1"));
-          System.out.println(req.queryParams("input2"));
+          //System.out.println(req.queryParams("input1"));
+          //System.out.println(req.queryParams("input2"));
+
           String input1 = req.queryParams("input1");
           java.util.Scanner sc1 = new java.util.Scanner(input1);
           sc1.useDelimiter("[;\r\n]+");
           java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-          while (sc1.hasNext()){
+          while (sc1.hasNext())
+          {
             int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
             inputList.add(value);
           }
@@ -47,13 +49,14 @@ public class App{
 
           boolean result = App.search(inputList, input2AsInt);
 
-          Map map = new HashMap();
+         Map map = new HashMap();
           map.put("result", result);
           return new ModelAndView(map, "compute.mustache");
         }, new MustacheTemplateEngine());
 
 
-        get("/compute",(rq, rs) -> {
+        get("/compute",
+            (rq, rs) -> {
               Map map = new HashMap();
               map.put("result", "not computed yet!");
               return new ModelAndView(map, "compute.mustache");
